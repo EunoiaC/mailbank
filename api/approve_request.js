8909a340ae92307cee84a2849f5c09db2a858914
Aadi Yadav <68039511+EunoiaC@users.noreply.github.com>
2025-10-12 00:38:03 -0700

    Update approve_request.js

--- api/approve_request.js
+++ api/approve_request.js
@@ -5,6 +5,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
     });
 }
 
